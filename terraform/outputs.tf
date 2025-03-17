a2ea92eeb9b26517d62f60655cf69ae22e38f73c
bhavikam28 <bhavi.28.mantri@gmail.com>
2025-03-17 10:55:48 -0500

    update outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -18,7 +18,3 @@ output "instance_public_ip" {
   value       = aws_instance.ec2_instance.public_ip
 }
 
-output "ec2_instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.ec2_instance.id
-}
