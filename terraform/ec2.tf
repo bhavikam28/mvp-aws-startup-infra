f32084a6892d53cd3e9d49a701fc405a84a62de8
bhavikam28 <bhavi.28.mantri@gmail.com>
2025-03-24 14:33:13 -0500

    updated ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,47 +1,42 @@
 # Security Group for EC2 Instance
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2-instance-sg"
-  description = "Allow HTTP and PostgreSQL traffic"
+  description = "Allow HTTP traffic"
   vpc_id      = var.vpc_id
 
-  # Allow HTTP traffic
+  # Allow HTTP traffic (required by assignment)
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP access"
   }
 
-  # Allow PostgreSQL traffic from the DMS replication instance
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.dms_sg.id]
-  }
-
-  # Allow all outbound traffic (can be restricted further if needed)
+  # Allow all outbound traffic (required by assignment)
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "ec2-instance-sg"
   }
 }
 
 
 # EC2 Instance
 resource "aws_instance" "ec2_instance" {
-  ami                    = var.custom_ami_version
-  instance_type          = "t2.micro"
-  subnet_id              = var.public_subnets[0] # Use the first public subnet
+  ami                    = data.aws_ami.custom_ami.id  # Using AMI data source
+  instance_type          = "t2.micro"                 # Required by assignment
+  subnet_id              = var.public_subnets[0]       # Required: using first public subnet
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-  associate_public_ip_address = true
-
-  # Attach the IAM Instance Profile for SSM
-  iam_instance_profile = aws_iam_instance_profile.ec2_ssm.name
+  associate_public_ip_address = true                  # Required by assignment
 
   tags = {
-    Name = "ec2-instance"
+    Name = "ec2-instance-${var.custom_ami_version}"   # Include AMI version in name
   }
 }
