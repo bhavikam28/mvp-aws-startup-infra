0da27f345bcd2704ef0632b0c2d1cb173987c9c6
bhavikam28 <bhavi.28.mantri@gmail.com>
2025-03-24 13:49:15 -0500

    updated tf files

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.startup_image_bucket.bucket
-}
-
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.cf_s3_distribution.id
-}
