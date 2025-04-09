ba6307c0d22e20398b8da73e10d042b7ae24d09f
bhavikam28 <bhavi.28.mantri@gmail.com>
2025-04-09 12:26:46 -0500

    chore: updated security rules

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -12,14 +12,6 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]  # Public access to ALB
   }
 
-  # Allow internal ALB→EC2 traffic 
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id]  
-  }
-
   # Allow all outbound traffic 
   egress {
     from_port   = 0
