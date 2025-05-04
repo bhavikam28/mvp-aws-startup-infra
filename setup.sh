606e3b19b1fa8bc9bdc675c58440c956976d1c71
bhavikam28 <bhavi.28.mantri@gmail.com>
2025-05-04 16:59:49 -0500

    chore: added CW agent config

--- setup.sh
+++ setup.sh
@@ -111,3 +111,72 @@ sudo ufw allow http
 
 # Print completion message
 echo "Django application setup complete!"
+
+
+#################################################################################################
+# CloudWatch Agent Installation and Configuration
+#
+# Relevant links:
+# https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/download-cloudwatch-agent-commandline.html
+# https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Agent-Configuration-File-Details.html
+#################################################################################################
+
+# Download and install CloudWatch Agent
+sudo wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+rm ./amazon-cloudwatch-agent.deb
+
+
+# Create CloudWatch Agent configuration
+sudo mkdir -p $APP_DIR
+sudo cat > $APP_DIR/cloudwatch-config.json <<EOF
+{
+  "agent": {
+     "metrics_collection_interval": 60,
+     "region": "eu-west-1"
+    },
+  "metrics": {
+      "append_dimensions": {
+      "ImageId": "\${aws:ImageId}",
+      "InstanceId": "\${aws:InstanceId}",
+      "InstanceType": "\${aws:InstanceType}",
+      "AutoScalingGroupName": "\${aws:AutoScalingGroupName}"
+      },
+        "metrics_collected": {
+    "mem": {
+        "measurement": [
+            { "name": "used_percent", "rename": "MEM_USAGE_PERCENT", "unit": "Percent" }
+          ]
+        }
+      }
+  },
+    "logs": {
+        "logs_collected": {
+          "files": {
+            "collect_list": [
+              {
+                "file_path": "/opt/app/**.log",
+                "log_group_name": "/startup/app",
+                "log_stream_name": "{instance_id}",
+                "retention_in_days": 30
+              }
+            ]
+          }
+      }
+    }
+}
+EOF
+
+
+# Start and enable CloudWatch Agent
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -s \
+    -c file:$APP_DIR/cloudwatch-config.json
+
+sudo systemctl enable amazon-cloudwatch-agent.service --now
+
+
+# Print completion message
+echo "CloudWatch Agent installation and configuration complete!"
